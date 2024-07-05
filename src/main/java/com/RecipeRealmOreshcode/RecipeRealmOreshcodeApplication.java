1852f77a45ff8c6532e9f4bcb80602159dab86b7
Oreshcode <Oreshcode@gmail.com>
2024-07-05 23:33:28 +0300

    Project Start

---
+++ src/main/java/com/RecipeRealmOreshcode/RecipeRealmOreshcodeApplication.java
@@ -0,0 +1,13 @@
+package com.RecipeRealmOreshcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeRealmOreshcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeRealmOreshcodeApplication.class, args);
+	}
+
+}
